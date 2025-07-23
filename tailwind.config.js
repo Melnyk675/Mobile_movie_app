1267ed119a5e95282a5ddfb5736b4d0d907df6fd
Dmytro Melnyk <magellan2402@gmail.com>
2025-07-23 19:01:20 +0300

    adding fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014", 
+        accent: "#AB8BFF"
+    },
   },
+},
   plugins: [],
 }
 
