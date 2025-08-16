afd2b7b5ffaf286ddcd870db40d82005569bd724
Dmytro Melnyk <magellan2402@gmail.com>
2025-08-17 00:34:47 +0300

    displaying film titles by fetching data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,21 @@
-import { View, Image, ScrollView } from "react-native";
-import { icons } from "@/constants/icons";
-import { images } from "@/constants/images";
+import { View, Image, Text, ScrollView, ActivityIndicator, FlatList } from "react-native";
+import useFetch from "@/services/useFetch";
 import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
+import { fetchMovies } from "@/services/api";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 
 export default function Index() {
   const router = useRouter();
+
+  const { 
+    data: movies, 
+    loading: moviesLoading, 
+    error: moviesError 
+  } = useFetch(() => fetchMovies({ 
+       query: "" 
+    }));
 
   return (
     <View className="flex-1 bg-primary">
@@ -24,7 +34,46 @@ export default function Index() {
              className="w-12 h-10 mt-20 mb-5 mx-auto"
            />
 
-           <View>
+           {moviesLoading ? (
+              <ActivityIndicator 
+                size="large"
+                color="#0000ff"
+                className="mt-10 self-center"
+              />
+           ): moviesError ? (
+               <Text>Error: {moviesError?.message}</Text>
+           ) : (
+              <View className="flex-1 mt-5">
+                 <SearchBar 
+                   onPress={() => router.push('/search')}
+                   placeholder="Search for a movie"
+                 />
+
+                 <>
+                   <Text className="text-lg text-white font-bold 
+                    mt-5 mb-3">Latest Movies</Text>
+
+                    <FlatList 
+                        data={movies}
+                        renderItem={({ item }) => (
+                         <Text className="text-white text-sm">{item.title}</Text>
+                     )}
+                        keyExtractor={(item) => item.id.toString()}
+                        numColumns={3}
+                        columnWrapperStyle={{ 
+                           justifyContent: 'flex-start',
+                           gap: 20,
+                           paddingRight: 5,
+                           marginBottom: 10 
+                        }}
+                        className="mt-2 pb-32"
+                        scrollEnabled={false}
+                   />
+                 </>
+              </View>
+           )}
+
+           <View className="flex-1 mt-5">
              <SearchBar 
                 onPress={() => router.push('/search')}
                 placeholder="Search for a movie"
@@ -34,3 +83,5 @@ export default function Index() {
     </View>
   );
 }
+
+
